2dfd4652f8955d1dc2b265705f74539f3edc94b8
bsdc-Cole <30273497@bsdc.ac.uk>
2024-10-08 17:10:30 +0100

    Icon Buttons

---
+++ script.js
@@ -0,0 +1,29 @@
+const discord=document.getElementById ("discord")
+
+discord.addEventListener("click", function(){
+    window.location.href="http://www.discord.com"
+});
+
+const instagram=document.getElementById ("instagram")
+
+instagram.addEventListener("click", function(){
+    window.location.href="http://www.instagram.com"
+});
+
+const twitter=document.getElementById ("twitter")
+
+twitter.addEventListener("click", function(){
+    window.location.href="http://www.twitter.com"
+});
+
+const twitch=document.getElementById ("twitch")
+
+twitch.addEventListener("click", function(){
+    window.location.href="http://www.twitch.com"
+});
+
+const youtube=document.getElementById ("youtube")
+
+youtube.addEventListener("click", function(){
+    window.location.href="http://www.youtube.com"
+});
