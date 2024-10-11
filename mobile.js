b21df8bb606413144372c0ecd41ae06606f42198
bsdc-Cole <30273497@bsdc.ac.uk>
2024-10-11 17:14:55 +0100

    mobile jarva script

---
+++ mobile.js
@@ -0,0 +1,30 @@
+const discord=document.getElementById ("discord")
+
+discord.addEventListener("click", function(){
+    window.location.href="https://discord.gg/Zts2eUBy"
+});
+
+const instagram=document.getElementById ("instagram")
+
+instagram.addEventListener("click", function(){
+    window.location.href="http://www.instagram.com"
+});
+
+const twitter=document.getElementById ("twitter")
+
+twitter.addEventListener("click", function(){
+    window.location.href="http://www.twitter.com"
+});
+
+const twitch=document.getElementById ("twitch")
+
+twitch.addEventListener("click", function(){
+    window.location.href="http://www.twitch.com"
+});
+
+const youtube=document.getElementById ("youtube")
+
+youtube.addEventListener("click", function(){
+    window.location.href="http://www.youtube.com"
+});
+  
